b143c58740c4cee3359d10ed8ba1128e79bc3378
hsahu11 <hsahu@xebia.com>
2018-01-09 16:17:22 +0530

    2nd commind

---
+++ om/jj.cpp
@@ -0,0 +1,18 @@
+/*
+ * jj.cpp
+ *
+ *  Created on: Jan 9, 2018
+ *      Author: hsahu
+ */
+
+#include "jj.h"
+
+jj::jj() {
+	// TODO Auto-generated constructor stub
+
+}
+
+jj::~jj() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ om/jj.h
@@ -0,0 +1,17 @@
+/*
+ * jj.h
+ *
+ *  Created on: Jan 9, 2018
+ *      Author: hsahu
+ */
+
+#ifndef JJ_H_
+#define JJ_H_
+
+class jj {
+public:
+	jj();
+	virtual ~jj();
+};
+
+#endif /* JJ_H_ */
